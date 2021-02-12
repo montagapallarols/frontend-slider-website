f4d0869bed4d4932b366c8d80401f204f6dcb679
montipallarols <montaga.pallarols@gmail.com>
2021-02-12 22:16:50 +0100

    Finish slide right functionality

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ setTimeout(function showIntro(){
 
 const slider = document.querySelector(".slider");
 const image = document.querySelector(".slide");
+const imageWidth = image.width
+// Image width 12226
 
 // Amount to move for each slide
 const slideDistances = [800, 1330, 2190, 3070, 3900, 4750, 6000, 6000, 6900]
@@ -22,60 +24,76 @@ const page2 = document.querySelector(".content-slide2")
 const page3 = document.querySelector(".content-slide3")
 const page4 = document.querySelector(".content-slide4")
 const page5 = document.querySelector(".content-slide5")
+const page6 = document.querySelector(".content-slide6")
+const page7 = document.querySelector(".content-slide7")
+const page8 = document.querySelector(".content-slide8")
+const page9 = document.querySelector(".content-slide9")
 
 const nav = document.querySelector(".nav")
 const navs = Array.from(nav.children)
 
 function moveRight() {
+    page0.classList.add("fade-out-text")
     if (currentSlide === 0) {
         image.style.transform = `translateX(-${slideDistances[0]}px)`
-        page1.style.display = "block"
+        page0.classList.remove("current")
+        page1.classList.add("current")
         navs[0].classList.remove("current-page")
         navs[1].classList.add("current-page")
     }
     if (currentSlide === 1) {
         image.style.transform = `translateX(-${slideDistances[1]}px)`
-        page1.classList.remove("fade-in-text")
-        page1.classList.add("fade-out-text")
-        page2.classList.add("fade-in-text")
-        page2.style.display = "block"
+        page1.classList.remove("current")
+        page2.classList.add("current")
         navs[1].classList.remove("current-page")
         navs[2].classList.add("current-page")
     }
     if (currentSlide === 2) {
         image.style.transform = `translateX(-${slideDistances[2]}px)`
-        page2.classList.remove("fade-in-text")
-        page2.classList.add("fade-out-text")
+        page2.classList.remove("current")
+        page3.classList.add("current")
         navs[2].classList.remove("current-page")
         navs[3].classList.add("current-page")
     }
     if (currentSlide === 3) {
         image.style.transform = `translateX(-${slideDistances[3]}px)`
+        page3.classList.remove("current")
+        page4.classList.add("current")
         navs[3].classList.remove("current-page")
         navs[4].classList.add("current-page")
     }
     if (currentSlide === 4) {
         image.style.transform = `translateX(-${slideDistances[4]}px)`
+        page4.classList.remove("current")
+        page5.classList.add("current")
         navs[4].classList.remove("current-page")
         navs[5].classList.add("current-page")
     }
     if (currentSlide === 5) {
         image.style.transform = `translateX(-${slideDistances[5]}px)`
+        page5.classList.remove("current")
+        page6.classList.add("current")
         navs[5].classList.remove("current-page")
         navs[6].classList.add("current-page")
     }
     if (currentSlide === 6) {
         image.style.transform = `translateX(-${slideDistances[6]}px)`
+        page6.classList.remove("current")
+        page7.classList.add("current")
         navs[6].classList.remove("current-page")
         navs[7].classList.add("current-page")
     }
     if (currentSlide === 7) {
         image.style.transform = `translateX(-${slideDistances[7]}px)`
+        page7.classList.remove("current")
+        page8.classList.add("current")
         navs[7].classList.remove("current-page")
         navs[8].classList.add("current-page")
     }
     if (currentSlide === 8) {
         image.style.transform = `translateX(-${slideDistances[8]}px)`
+        page8.classList.remove("current")
+        page9.classList.add("current")
         arrowRight.style.display = "none"
         navs[8].classList.remove("current-page")
         navs[9].classList.add("current-page")
@@ -84,10 +102,19 @@ function moveRight() {
 
 function moveLeft() {
     if (currentSlide === 1) {
-        image.style.transform = `translateX(${slide2}px)`
+        image.style.transform = `translateX(${0}px)`
+        page1.classList.remove("current")
+        page0.classList.add("current")
+        arrowLeft.style.display = "none"
+        navs[1].classList.remove("current-page")
+        navs[0].classList.add("current-page")
     }
     if (currentSlide === 2) {
-        image.style.transform = `translateX(${slide3}px)`
+        image.style.transform = `translateX(-${slideDistances[0]}px)`
+        page2.classList.remove("current")
+        page1.classList.add("current")
+        navs[2].classList.remove("current-page")
+        navs[1].classList.add("current-page")
     }
     if (currentSlide === 3) {
         image.style.transform = `translateX(${slide4}px)`
@@ -109,13 +136,14 @@ function moveLeft() {
     }
 }
 
+
 arrowRight.addEventListener("click", () => {
-    page0.classList.add("fade-out-text")
     moveRight()
     arrowLeft.style.display = "block";
     currentSlide++
     console.log(currentSlide)
 })
+
 
 arrowLeft.addEventListener("click", () => {
     moveLeft()
